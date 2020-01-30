9aad7436a75a4ff10676a3411a81ba362be034da
Hugo Meza <hgmeza114@gmail.com>
2020-01-29 19:26:18 -0500

    implemented email/passw auth using firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import AuthenticationPage from './pages/authentication/authentication'
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -21,8 +21,21 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = createUserProfileDocument(userAuth);
+
+        (await userRef).onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      } else {
+       this.setState({currentUser: userAuth}); 
+      }
     })
   }
 
--- src/pages/authentication/authentication.js
+++ src/pages/authentication/authentication.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SignIn from './../../components/SignIn/SignIn';
+import SignUp from './../../components/SignUp/SignUp';
 
 import './authentication.scss';
 
@@ -8,6 +9,7 @@ const AuthenticationPage = () => {
   return ( 
     <div className='authentication'>
       <SignIn />
+      <SignUp />
     </div>
    );
 }
