395698b0b5cd132de57e365255bc59afecf7b5d5
Hugo Meza <hgmeza114@gmail.com>
2020-02-01 22:01:50 -0500

    integrated stripe for mock payments

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -3,6 +3,8 @@ import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 
 import CheckoutItem from './../../components/CheckoutItem/CheckoutItem';
+import StripeCheckoutButton from './../../components/StripeButton/StripeButton';
+
 import {selectCartItems, selectCartTotal} from './../../redux/cart/cart.selectors';
 
 import './checkout.scss';
@@ -32,9 +34,13 @@ const CheckoutPage = ({cartItems, total}) => {
           <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         )
       }
-      <div className='total'>
-        <span>TOTAL ${total}</span>
+      <div className='total'>TOTAL ${total}</div>
+      <div className='test-warning'>
+        *Use the following test credit card for payments*
+        <br/>
+        4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
       </div>
+      <StripeCheckoutButton price={total} />
     </div>
    );
 }
